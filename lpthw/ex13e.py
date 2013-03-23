95689cff38a2c997de83a2144946620bb5025cfe
lijiali1226 <lijiali1226@gmail.com>
2013-03-23 14:50:05 +0800

    day2

---
+++ lpthw/ex13e.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+print "Your forth variable is:", raw_input()
+print "Your fifth variable is:", raw_input()
