6cb3785e029c97d8fe3212e19a1c7cc4cb856811
lijiali1226 <lijiali1226@gmail.com>
2013-03-29 19:34:31 +0800

    day5

---
+++ lpthw/ex-review.py
@@ -0,0 +1,79 @@
+split(' ') pop(0) pop(-1) sorted() append()  ' '.join() '#'.join() 
+
+elements = []
+for i in elements:
+elements[0]
+elements[-1]
+elements[3:5]
+
+elements = {}
+elements['a'] = 1
+del elements['a']
+
+for i in range(0, 6):
+
+from sys import exit
+exit(0)
+exit(1)
+from random import randint
+randint(0,len(a)-1)
+
+def func(a,b):
+    ...
+
+elements['_fun'] = func
+
+while True:
+    print "INPUT?(ENTER to quit)"
+    Input = raw_input("> ")
+    if not Input: break
+    
+    func_value = elements['_fun'](a,b)
+    print func_value          
+
+
+class thing(object):
+    def __init__(self):
+        self.number = 0
+    def func(self):
+        print " ... "
+    def morefunc(self, more):
+        self.number += more
+        return self.number
+a = thing()
+b = thing()
+a.func()
+print a.morefunc(1)
+print a.number
+
+getattr(self, next)
+get()
+update()
+super()
+strip()
+replace()
+
+import random
+from urllib import urlopen
+import sys
+random.sample()
+random.shuffle()
+
+def convert_number(s)
+    try:
+        return int(s)
+    except ValueError
+        return None
+
+class ParserError(Exception):
+    pass
+
+raise ParserEroor("...")
+
+#nosetests
+from nose.tools import *
+assert_equal()
+assert_raises()
+assert_raises_regexp()
+assert
+
