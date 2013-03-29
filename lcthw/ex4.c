9806e74241f6f868d90332a98391344d802cb440
lijiali1226 <lijiali1226@gmail.com>
2013-03-29 22:43:11 +0800

    day1

--- lcthw/ex4.c
+++ lcthw/ex4.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/* Warning : This program is wrong on purpose. */
+/* Fixed */
 
 int main()
 {
 	int age = 10;
-	int height;
+	int height = 72;
 
-	printf("I am %d years old.\n");
+	printf("I am %d years old.\n", age);
 	printf("I am %d inches tall.\n", height);
 	
 	return 0;
