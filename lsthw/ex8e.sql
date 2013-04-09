4e058a402d948924cb46fcd7164e99ae1f54f575
lijiali1226 <lijiali1226@gmail.com>
2013-04-10 03:48:50 +0800

    adjust

--- lsthw/ex8e.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM person;
-SELECT * FROM pet;
-SELECT * FROM person_pet;
-
-DELETE FROM person_pet
-    WHERE pet_id IN(
-        SELECT id FROM pet WHERE dead = 0
-    );
-SELECT * FROM person_pet;
